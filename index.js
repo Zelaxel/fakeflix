531adff25429fce23f71073dcdcf0fadfce0ecf2
Jesus Toro <jesus.tdd03@gmail.com>
2026-02-18 17:30:15 +0000

    Implemented header in index (sample code)

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-
+document.addEventListener("DOMContentLoaded", () => {
+    fetch('components/header/header.html')
+        .then(response => {
+            if (!response.ok) throw new Error("No se pudo cargar el header");
+            return response.text();
+        })
+        .then(data => {
+            document.getElementById('header-placeholder').innerHTML = data;
+        })
+        .catch(error => console.error("Error cargando el componente:", error));
+});
