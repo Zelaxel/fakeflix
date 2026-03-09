7bdb3001b32ba7f61b2da71eba778f82c604a862
Jesus Toro <jesus.tdd03@gmail.com>
2026-03-09 20:01:08 +0000

    Refactored homepage to index

--- js/common.js
+++ js/common.js
@@ -1,6 +1,6 @@
 async function loadTemplate(templateName, targetElementId) {
     try {
-        const response = await fetch(`/pages/${templateName}.html`);
+        const response = await fetch(`/components/${templateName}.html`);
         const html = await response.text();
         document.getElementById(targetElementId).innerHTML = html;
     } catch (error) {
