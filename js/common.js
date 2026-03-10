1ec261dc82ebab677f9b31427fcee9621e32f102
Jesus Toro <jesus.tdd03@gmail.com>
2026-03-10 16:34:46 +0000

    Popular items are loaded from data

--- js/common.js
+++ js/common.js
@@ -8,6 +8,22 @@ async function loadTemplate(templateName, targetElementId) {
     }
 }
 
-function fetchData(category) {
+async function getTemplate(templateName) {
+    try {
+        const response = await fetch(`/components/${templateName}.html`);
+        return await response.text();
+    } catch (error) {
+        console.error(`Error loading template ${templateName}:`, error);
+    }
+}
 
+
+async function fetchPopularTitles() {
+    try {
+        const response = await fetch("/data/popular-titles.json");
+        return await response.json();
+    } catch (e) {
+        console.error("Error fetching data: ", e);
+        return null;
+    }
 }
--- js/home.js
+++ js/home.js
@@ -1,3 +1,20 @@
 document.addEventListener('DOMContentLoaded', async () => {
     await loadTemplate("header", "header");
+
+    const data = await fetchPopularTitles();
+    if (!data) return;
+
+    const popular = [...data.popular]
+    
+    const container = document.getElementById("title-list-items");
+    const template = await getTemplate("title-list-item");
+
+    popular.forEach(title => {
+        const item = template
+            .replace(/{{title}}/g, title.title)
+            .replace(/{{image}}/g, title.image);
+        console.log(item);
+        container.insertAdjacentHTML("beforeend", item);
+    })
+
 });
