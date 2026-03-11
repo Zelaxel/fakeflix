6ff80f9f8744fdcb73732abea0338e40912d15d7
Jesus Toro <jesus.tdd03@gmail.com>
2026-03-11 17:46:30 +0000

    Implemented loading recommended title from json

--- js/home.js
+++ js/home.js
@@ -4,6 +4,14 @@ document.addEventListener('DOMContentLoaded', async () => {
     const data = await fetchPopularTitles();
     if (!data) return;
 
+    const recommended = data.main;
+    const html = document.getElementById("recommended").innerHTML
+                            .replace(/{{title}}/g, recommended.title)
+                            .replace(/{{description}}/g, recommended.info)
+                            .replace(/{{image}}/g, recommended.image);
+    console.log(html);
+    document.getElementById("recommended").innerHTML = html;
+
     const popular = [...data.popular]
     
     const container = document.getElementById("title-list-items");
