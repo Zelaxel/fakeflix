4e2607db3eeae93534d55df826052551f24569b2
Edwin <edwinob36@gmail.com>
2026-03-16 21:04:02 +0000

    Los usarios registrados pueden iniciar sesion

--- js/login.js
+++ js/login.js
@@ -3,3 +3,32 @@ const email = params.get("email");
 if(email) {
     document.getElementById("signinEmail").value = email;
 }
+
+const form = document.querySelector("form");
+
+form.addEventListener("submit", async (e) => {
+    e.preventDefault();
+
+    const email = document.getElementById("signinEmail").value;
+    const password = document.querySelector("input[name='password']").value;
+
+    const res = await fetch("/data/users.json");
+    const data = await res.json();
+
+    const users = data.users;
+
+    const user = users.find(u => u.email === email);
+
+    if (!user) {
+        alert("Usuario no encontrado");
+        return;
+    }
+
+    if (user.password !== password) {
+        alert("Contraseña incorrecta");
+        return;
+    }
+
+    alert("Inicio de sesión exitoso");
+    window.location.href = "../index.html";
+});
