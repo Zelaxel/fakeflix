b756e466173776afc408422d3bdaa68507cf9603
Jesus Toro <jesus.tdd03@gmail.com>
2026-03-26 18:44:13 +0000

    Title details work correctly with series

--- js/details.js
+++ js/details.js
@@ -4,7 +4,6 @@ document.addEventListener("DOMContentLoaded", async () => {
     const id = Number(new URLSearchParams(window.location.search).get("id"));
     const data = await fetchTitles();
     if (!data) return;
-
     const title = [...data.titles].find(title => title.id === id);
     
     const element = document.getElementById("details-container");
@@ -16,9 +15,11 @@ document.addEventListener("DOMContentLoaded", async () => {
     videoPreview.innerHTML = videoPreview.innerHTML
                                 .replace(/{{image}}/g, title.image);
 
+    const playButton = document.getElementById("play");
     const seasonSection = document.getElementById("seasons");
     if (title.type === "movie") {
         seasonSection.innerHTML = "";
+        videoPreview.innerHTML = videoPreview.innerHTML.replace(/{{video}}/g, title.video)
     } else {
         const seasonTemplate = await getTemplate("season");
         const episodeTemplate = await getTemplate("season-episode");
@@ -29,11 +30,22 @@ document.addEventListener("DOMContentLoaded", async () => {
             const epList = newSeason.querySelector("#episode-list");
 
             [...season.episodes].forEach(ep => {
-                const newEp = episodeTemplate.replace(/{{name}}/g, ep.title);
-                epList.insertAdjacentHTML("beforeend", newEp);
+                const newEp = document.createElement("div");
+                newEp.innerHTML = episodeTemplate
+                                            .replace(/{{name}}/g, ep.title)
+                                            .replace(/{{video}}/g, ep.video);
+
+                
+                newEp.firstElementChild.addEventListener("click", () => {
+                    playButton.href = newEp.firstElementChild.getAttribute("href");
+                });
+                
+                epList.appendChild(newEp);
             });
 
             seasonSection.appendChild(newSeason.firstElementChild);
         });
+
+        playButton.href = playButton.href.replace(/{{video}}/g, title.seasons[0].episodes[0].video);
     }
 });
