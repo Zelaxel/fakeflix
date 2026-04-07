5a924e9bf14895a9110ac479332a30dcb53f9591
Zelaxel <alejandroramirezlorenzo2003@gmail.com>
2026-04-07 12:55:30 +0100

    Migration to angular

--- js/check_sesion.js
+++
@@ -1,4 +0,0 @@
-window.addEventListener("pageshow", async () => {
-    const session = localStorage.getItem("isLoggedIn");
-    if(session !== "true") window.location.href = "./homepage-guest.html";
-})
