af63d8b51d01de8f7221b526fa7b049040306123
Jesus Toro <jesus.tdd03@gmail.com>
2026-04-14 16:25:30 +0100

    Added homepage-guest to routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
 
 export const routes: Routes = [
-    {path: '', loadComponent: () => import("./components/homepage/homepage").then(m => m.Homepage)},
+    {path: '', loadComponent: () => import("./components/homepage-guest/homepage-guest").then(m => m.HomepageGuest)},
+    {path: 'home', loadComponent: () => import("./components/homepage/homepage").then(m => m.Homepage)},
     {path: 'list', loadComponent: () => import('./components/title-grid/title-grid').then(m => m.TitleGrid)}
 ];
