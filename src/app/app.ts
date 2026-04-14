f754cd5bd9385a9e0b448b184c90364147431c98
Jesus Toro <jesus.tdd03@gmail.com>
2026-04-14 15:53:27 +0100

    Implemented homepage

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,10 +2,11 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { TitleItem } from './components/title-item/title-item'
 import { Header } from './components/header/header'
+import { Homepage } from './components/homepage/homepage'
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, TitleItem, Header],
+  imports: [RouterOutlet, TitleItem, Header, Homepage],
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
---
+++ src/app/components/homepage/homepage.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { Header } from '../header/header';
+
+@Component({
+  selector: 'app-homepage',
+  imports: [Header],
+  templateUrl: './homepage.html',
+  styleUrl: './homepage.css',
+})
+export class Homepage {}
