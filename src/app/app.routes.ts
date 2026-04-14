3d2bd685fb7e5f57534ed6eef533ee0a408634c2
Alejandro <alejandroramirezlorenzo2003@gmail.com>
2026-04-14 16:24:19 +0100

    Minor fix

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
 
 export const routes: Routes = [
-    { path: '', loadComponent: () => import('./components/title-grid/title-grid').then(m => m.TitleGrid)},
-    {path: "", loadComponent: () => import("./components/homepage/homepage").then(m => m.Homepage)}
+    { path: 'titles', loadComponent: () => import('./components/title-grid/title-grid').then(m => m.TitleGrid)},
+    { path: 'home', loadComponent: () => import("./components/homepage/homepage").then(m => m.Homepage)},
+    { path: '', redirectTo: 'home', pathMatch: 'full'}
 ];
