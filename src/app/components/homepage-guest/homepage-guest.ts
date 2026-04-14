8999cba841eccef8487b5892198687167520c09d
Jesus Toro <jesus.tdd03@gmail.com>
2026-04-14 16:24:05 +0100

    Generated component homepage-guest

---
+++ src/app/components/homepage-guest/homepage-guest.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-homepage-guest',
+  imports: [],
+  templateUrl: './homepage-guest.html',
+  styleUrl: './homepage-guest.css',
+})
+export class HomepageGuest {}
