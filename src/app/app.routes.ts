c9902d76a6576673bc2dfae86acba52a5312339b
Jesus Toro <jesus.tdd03@gmail.com>
2026-04-14 16:21:44 +0100

    Fixed homepage texts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,6 @@
 import { Routes } from '@angular/router';
 
 export const routes: Routes = [
-    { path: '', loadComponent: () => import('./components/title-grid/title-grid').then(m => m.TitleGrid)},
-    {path: "", loadComponent: () => import("./components/homepage/homepage").then(m => m.Homepage)}
+    {path: '', loadComponent: () => import("./components/homepage/homepage").then(m => m.Homepage)},
+    {path: 'list', loadComponent: () => import('./components/title-grid/title-grid').then(m => m.TitleGrid)}
 ];
