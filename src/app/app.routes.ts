3bbe11fba6d9d47a7855e27b2a9eca953bb1ad7e
Alejandro <alejandroramirezlorenzo2003@gmail.com>
2026-04-21 15:02:38 +0100

    Working on user-panel

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,5 +3,6 @@ import { Routes } from '@angular/router';
 export const routes: Routes = [
     {path: '', loadComponent: () => import("./pages/homepage-guest/homepage-guest").then(m => m.HomepageGuest)},
     {path: 'home', loadComponent: () => import("./pages/homepage/homepage").then(m => m.Homepage)},
-    {path: 'titles', loadComponent: () => import('./pages/title-grid/title-grid').then(m => m.TitleGrid)}
+    {path: 'titles', loadComponent: () => import('./pages/title-grid/title-grid').then(m => m.TitleGrid)},
+    {path: 'user-panel', loadComponent: () => import('./components/user-panel/user-panel').then(m => m.UserPanel)}
 ];
---
+++ src/app/components/user-panel/user-panel.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  standalone: true,
+  selector: 'app-user-panel',
+  imports: [],
+  templateUrl: './user-panel.html',
+  styleUrl: './user-panel.css',
+})
+export class UserPanel {}
