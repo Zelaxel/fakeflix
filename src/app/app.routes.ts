4899e4a11bc59a146ab615117de5dfdef669ad0b
Jesus Toro <jesus.tdd03@gmail.com>
2026-04-21 15:12:14 +0100

    Implemented title details page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,5 +3,7 @@ import { Routes } from '@angular/router';
 export const routes: Routes = [
     {path: '', loadComponent: () => import("./pages/homepage-guest/homepage-guest").then(m => m.HomepageGuest)},
     {path: 'home', loadComponent: () => import("./pages/homepage/homepage").then(m => m.Homepage)},
-    {path: 'titles', loadComponent: () => import('./pages/title-grid/title-grid').then(m => m.TitleGrid)}
+    {path: 'titles', loadComponent: () => import('./pages/title-grid/title-grid').then(m => m.TitleGrid)},
+    {path: 'details', loadComponent: () => import('./pages/title-details/title-details').then(m => m.TitleDetails)}
+
 ];
---
+++ src/app/pages/title-details/title-details.spec.ts
@@ -0,0 +1,22 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { TitleDetails } from './title-details';
+
+describe('TitleDetails', () => {
+  let component: TitleDetails;
+  let fixture: ComponentFixture<TitleDetails>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [TitleDetails],
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(TitleDetails);
+    component = fixture.componentInstance;
+    await fixture.whenStable();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
