cb2379f0b90b085b3e54e2b95e74762fa156ef32
Zelaxel <alejandroramirezlorenzo2003@gmail.com>
2026-05-03 02:07:13 +0100

    Added search

--- src/app/components/header/header.ts
+++ src/app/components/header/header.ts
@@ -1,6 +1,7 @@
-import { Component, ElementRef, viewChild, ViewChild } from '@angular/core';
-import { RouterLink } from "@angular/router";
+import { Component, ElementRef, inject, viewChild, ViewChild } from '@angular/core';
+import { RouterLink, Router } from "@angular/router";
 import { UserPanel } from '../user-panel/user-panel';
+import { query } from 'firebase/firestore';
 
 @Component({
   selector: 'app-header',
@@ -11,10 +12,22 @@ import { UserPanel } from '../user-panel/user-panel';
 export class Header {
   userPanelVisible: boolean = false;
 
+  router = inject(Router);
+
+  @ViewChild('search-text') input!: ElementRef<HTMLInputElement>;
+
   showUserPanel(): void {
     this.userPanelVisible = true;
   }
   hideUserPanel(): void {
     this.userPanelVisible = false;
+  }
+
+  search(value: string) {
+    if (value !== '') {
+      this.router.navigate(['/titles'], {
+        queryParams: {query: value, showing: value}
+      });
+    }
   }
 }
